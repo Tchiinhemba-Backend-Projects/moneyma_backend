726a15e34db64718f535d3f989b3c41ed9874508
tchiinhemba <tchiinhemba@gmail.com>
2025-07-27 16:38:31 +0100

    initial commit

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+import app from "./app";
+
+const port = process.env.APP_PORT;
+app.listen(port, () => {
+    console.log(`Server is running at: http://localhost:${port}`);
+})
